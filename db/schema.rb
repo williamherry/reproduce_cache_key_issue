dbb672b789fa3dbe11f5d45513df42efbb643ee0
William Herry <WilliamHerryChina@Gmail.com>
2014-05-26 09:16:25 +0800

    add tag scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140526011536) do
+ActiveRecord::Schema.define(version: 20140526011609) do
 
   create_table "courses", force: true do |t|
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tags", force: true do |t|
     t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
